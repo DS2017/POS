a83a7b3f80877130fd891a5571879a4f7ce587b8
DS2017 <deeptis.saxena@gmail.com>
2023-02-24 16:23:27 +0530

    Create rootReducer.js

---
+++ client/src/redux/rootReducer.js
@@ -0,0 +1,40 @@
+const initialState ={
+    loading:false,
+    cartItem:[]
+}
+
+export const rootReducer = (state=initialState,action) => {
+ switch(action.type){
+    case 'SHOW_LOADING':
+        return{
+            ...state,
+            loading:true
+        }
+    case 'HIDE_LOADING':
+        return{
+            ...state,
+            loading:false
+        }
+    case 'ADD_TO_CART':
+        return {...state, 
+            cartItem:[...state.cartItem,action.payload]
+        }
+    case 'UPDATE_CART':
+        return {
+            ...state,
+            cartItem: state.cartItem.map((item) => 
+                item._id === action.payload._id
+                ? {...item, quantity: action.payload.quantity}
+                : item
+            )
+        }
+    case 'DELETE_FROM_CART':
+        return{
+            ...state,
+            cartItem:state.cartItem.filter((item) => 
+                item._id !== action.payload._id  )
+        }
+    default:
+        return state;
+ }
+}
